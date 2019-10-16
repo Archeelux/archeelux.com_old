c45db57300a686e4e7a627b0e37cddea11eede53
Archeelux <archeelux@gmail.com>
2019-10-16 15:42:45 +0100

    fixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,31 @@
+const fg = require("fast-glob");
+
+// Search for Icons in icons folder
+const iconImages = fg.sync("src/images/icons/*", { objectMode: true });
+
+const output = [];
+for (let image of iconImages) {
+  const outputImage = {
+    file: image.path.replace("src/", "")
+  };
+  output.push(outputImage);
+}
+
+console.log(output);
+
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/images')
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  eleventyConfig.addFilter("dump", obj => {
+    return util.inspect(obj);
+  });
+
+  eleventyConfig.addCollection("icons", collection => output);
 
   return {
-    dir: { input: 'src', output: 'dist', data: '_data' },
+    dir: { input: "src", output: "dist", data: "_data" },
     passthroughFileCopy: true,
-    templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
-    htmlTemplateEngine: 'njk'
-  }
-}
+    templateFormats: ["njk", "md", "css", "html", "yml"],
+    htmlTemplateEngine: "njk"
+  };
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,16 @@
-import list from './modules/list'
+import {
+  library,
+  icon,
+  dom
+} from "../../node_modules/@fortawesome/fontawesome-svg-core";
 
-console.log(`Hello ${list[0]}`)
+import {
+  faInstagram,
+  faGithub,
+  faDev,
+  faLinkedin
+} from "../../node_modules/@fortawesome/free-brands-svg-icons";
+
+library.add(faInstagram, faGithub, faDev, faLinkedin);
+
+dom.i2svg();
